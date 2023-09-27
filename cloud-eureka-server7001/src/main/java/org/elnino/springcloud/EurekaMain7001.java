6d53e7af11200f441c45a3834cf7c83a4f138b49
El nino <el-nino202020@protonmail.com>
2023-09-27 11:30:39 +0800

    单机Eureka构建步骤

--- cloud-eureka-server7001/src/main/java/org/elnino/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/org/elnino/springcloud/EurekaMain7001.java
@@ -1,14 +1,13 @@
 package org.elnino.springcloud;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class MainApp80 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(MainApp80.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
