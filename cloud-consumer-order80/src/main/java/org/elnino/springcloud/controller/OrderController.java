824e73d40ebbd653aa1258b6cfe7bf5381d93020
El nino <el-nino202020@protonmail.com>
2023-09-27 15:31:40 +0800

    服务发现客户端

--- cloud-consumer-order80/src/main/java/org/elnino/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/org/elnino/springcloud/controller/OrderController.java
@@ -3,10 +3,14 @@ package org.elnino.springcloud.controller;
 import org.elnino.springcloud.entity.CommonResult;
 import org.elnino.springcloud.entity.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
@@ -15,6 +19,9 @@ public class OrderController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/consumer/payment/create") //客户端用浏览器是get请求，但是底层实质发送post调用服务端8001
     public CommonResult create(Payment payment) {
@@ -25,5 +32,21 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult getPayment(@PathVariable Long id) {
         return restTemplate.getForObject(PaymentSrv_URL + "/payment/get/" + id, CommonResult.class, id);
+    }
+
+    @GetMapping(value = "/consumer/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            System.out.println(service);
+            List<ServiceInstance> instances = discoveryClient.getInstances(service);
+            for (ServiceInstance instance : instances) {
+                System.out.println(instance.getInstanceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t"
+                        + instance.getUri());
+            }
+            System.out.println("============================");
+        }
+
+        return this.discoveryClient;
     }
 }
