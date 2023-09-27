f883a7ca4c984d03e1171d651dae40e7cde143e2
El nino <el-nino202020@protonmail.com>
2023-09-27 14:22:48 +0800

    集群 Eureka 构建步骤

--- cloud-consumer-order80/src/main/java/org/elnino/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/org/elnino/springcloud/controller/OrderController.java
@@ -11,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class OrderController {
 
-    public static final String PaymentSrv_URL = "http://localhost:8001";
+    public static final String PaymentSrv_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Autowired
     private RestTemplate restTemplate;
