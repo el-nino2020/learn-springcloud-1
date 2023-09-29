5afcdfd916cb9cba7d0d41baf8d1dfbb84813704
El nino <el-nino202020@protonmail.com>
2023-09-29 22:29:05 +0800

    Hystrix 断路器

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/org/elnino/springcloud/controller/OrderHystrixController.java
@@ -0,0 +1,40 @@
+package org.elnino.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.elnino.springcloud.service.PaymentHystrixService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+//@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
+public class OrderHystrixController {
+    @Autowired
+    private PaymentHystrixService paymentHystrixService;
+
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
+        String result = paymentHystrixService.paymentInfo_OK(id);
+        return result;
+    }
+
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+//    @HystrixCommand
+//    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500")
+//    })
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        String result = paymentHystrixService.paymentInfo_TimeOut(id);
+        return result;
+    }
+
+//    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id) {
+//        return "我是消费者80,对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
+//    }
+
+    public String payment_Global_FallbackMethod() {
+        return "Global异常处理信息，请稍后再试，/(ㄒoㄒ)/~~";
+    }
+}
